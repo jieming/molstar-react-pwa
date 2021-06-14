204bfe9a384def6eaeb9b72e5c34a8faa8bad1c8
Jieming Liu <jliu@exscientia.com>
2021-06-14 15:55:26 +0100

    Add new input

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,10 +1,22 @@
+import { Button, TextField } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
-import React, { useEffect } from 'react'
+
+import React, { useEffect, useState } from 'react'
 
 const useStyles = makeStyles(theme => ({
   root: {
     display: 'flex',
-    height: '100%'
+    height: '100%',
+    alignContent: 'center',
+    justifyContent: 'center'
+  },
+  inputRoot: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignSelf: 'center'
+  },
+  viewButton: {
+    marginTop: theme.spacing(1)
   }
 }))
 
@@ -12,12 +24,41 @@ const Home = () => {
   const classes = useStyles()
 
   useEffect(() => {
-    document.title = 'Exscientia | P.W.A.'
+    document.title = 'MolStar | P.W.A.'
   })
+
+  const [structureId, setStructureId] = useState('')
+  const [diplay3D, setDisplay3D] = useState(false)
+
+  const onIdChange = event => setStructureId(event.target.value)
+
+  const onViewClick = () => setDisplay3D(true)
+
+  const renderStructureIDInput = () => (
+    <div className={classes.inputRoot}>
+      <TextField
+        label='Structure ID'
+        placeholder='e.g. 2vta'
+        onChange={onIdChange}
+        value={structureId}
+      />
+      <Button
+        className={classes.viewButton}
+        variant='contained'
+        onClick={onViewClick}
+      >
+        3D View
+      </Button>
+    </div>
+  )
 
   return (
     <div className={classes.root}>
-      <pdbe-molstar molecule-id='1cbs'></pdbe-molstar>
+      {diplay3D ? (
+        <pdbe-molstar molecule-id={structureId} />
+      ) : (
+        renderStructureIDInput()
+      )}
     </div>
   )
 }
